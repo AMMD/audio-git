816d34fa15d2a0bb37fcecc7bea89dce5ad1adde
Merouane Atig <yahoocbien-dev@yahoo.fr>
2012-03-02 22:52:50 +0100

    Add text for new test

--- spec/audiogit_spec.rb
+++ spec/audiogit_spec.rb
@@ -1,11 +1,17 @@
 require_relative "../lib/audiogit/git_facade"
 
 describe GitFacade do
+    before { @facade = GitFacade.new }
+
     it "should create a git repository when it receives the create() message" do
-        facade = GitFacade.new
-        facade.create
+        @facade.create
         exist = FileTest.exist? ".audiogit"
         exist.should == true
         FileUtils.rm_rf ".audiogit"
+    end
+
+    it "should add and commit files listed in Ardour xml file" do
+        @facade.update "ardour.xml"
+        # list files commited to git => should be = to files in ardour.xml
     end
 end
